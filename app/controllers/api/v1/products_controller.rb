73e066e2a6fd1b771f0252762f92a18097e49cf4
00198216 <00198216@uca.edu.sv>
2020-05-28 07:09:16 -0600

    finished product CRUD

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -13,4 +13,37 @@ class Api::V1::ProductsController < ApplicationController
       def show
         @product = Product.find_by(id: params[:id])
       end
+
+      def create
+        if @current_user.is_Admin?
+        @product = Product.new(product_params)
+        if @product.save
+          render json: {status: "Product created successfully"}, status: 201
+          #ActionCable.server.broadcast 'super_channel', user: @current_user.lists
+    
+        else
+          render :json => { :errors => @product.errors.full_messages }, status: :bad_request
+        end
+      end
+    end
+
+    def update
+    
+        if @current_user.is_Admin?
+        @Product=Product.find_by(id: params[:id])
+  
+        if @Product.update(product_params)
+          render json: {status: "product updated"}, status: 201
+        else
+          render :json => { :errors => @product.errors.full_messages }, status: 400
+        end
+    end
+    
+    end
+
+      private
+      def product_params
+        params.require(:product).permit(:name,:category,:quantity,:photo)
+      end
+    
 end

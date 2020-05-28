d2ae202d7c3c94ee1b0b683bd5558bcb4569a0fd
00198216 <00198216@uca.edu.sv>
2020-05-28 03:57:36 -0600

    Started add products

--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ListsController < ApplicationController
-
+   
             rescue_from ActiveRecord::RecordNotFound, :with => :task_not_found
             before_action :authenticate_request!, except: [:index]
             before_action :load_current_user!, only: [:create]
@@ -16,16 +16,31 @@ def create
     @current_user.lists << @list
     if @list.save
       render json: {status: "List created successfully"}, status: 201
-      ActionCable.server.broadcast 'super_channel', user: @current_user.lists
+      #ActionCable.server.broadcast 'super_channel', user: @current_user.lists
 
     else
       render :json => { :errors => @list.errors.full_messages }, status: :bad_request
     end
   end
 
+  def add
+    @List = List.find_by(id: params[:id])
+    @product= Product.find_by(name: params[:name])
+    @List.products << @product
+    render json: {status: "added product successfully"}, status: 201
+  end
+
+
   private
   def list_params
     params.require(:list).permit(:name, :quantity)
   end
+
+
+  private
+  def product_params
+    params.require(:product).permit(:name)
+  end
+ 
 end
  
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       mount ActionCable.server => '/cable'
-      resources :lists
+      resources :lists  do
+        collection do
+          post 'add'
+        end
+      end
       resources :listusers
       resources :products, only: [:show,:index,:create] 
       resources :users, only: [:create,:index,:update,:show] do

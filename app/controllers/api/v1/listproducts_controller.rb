e20c754f4754705b190e7047ced1bebda5a85a0a
00198216 <00198216@uca.edu.sv>
2020-06-01 00:39:56 -0600

    Action cable fix

--- app/controllers/api/v1/listproducts_controller.rb
+++ app/controllers/api/v1/listproducts_controller.rb
@@ -25,8 +25,7 @@ end
       @association= ListProduct.find_by(
         list_id: @List.id,
         product_id: @product.id)
-      @association.quantity=params[:quantity]
-      @association.save()
+      @association.quantity<<params[:quantity]
       puts(@association.quantity)
 
       render json: {status: "added product successfully"}, status: 201

87ff7205e7e783de8150ada4f38453fd6b959c58
00198216 <00198216@uca.edu.sv>
2020-06-06 01:55:58 -0600

    List_User validations

--- app/models/list_user.rb
+++ app/models/list_user.rb
@@ -2,5 +2,5 @@ class ListUser < ApplicationRecord
     belongs_to :list
     belongs_to :user
 
-    #validates_uniqueness_of :list_id, :scope => [:user_id]
+    validates_uniqueness_of :list_id, :scope => [:user_id]
 end
